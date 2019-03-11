49b42a3117fac9bc51e2e91e78fe54e20ef48d98
Muryel Stachelski <muryelstachelski@gmail.com>
2019-03-11 15:11:27 -0300

    adiciona prototipos

--- matematica.c
+++ matematica.c
@@ -9,3 +9,11 @@ int sub(int a, int b){
 	int res= a-b;
 	return res;
 }
+
+int multi(int a, int b){
+	
+}
+
+int  quad(int a){
+	
+}
--- matematica.h
+++ matematica.h
@@ -2,3 +2,5 @@
 
 int soma(int a, int b);
 int sub(int a, int b);
+int mult(int a, int b);
+int quad(int a);
